c6f8af6251ce5d5cbe1bdb290bbe79971898d46c
Hari Shreedharan <hshreedharan@apache.org>
2014-09-18 16:26:19 -0700

    Initial Commit

---
+++ src/main/scala/benchmarktest/HDFSFileAppender.scala
@@ -0,0 +1,59 @@
+package benchmarktest
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.{Timer, TimerTask}
+
+import com.google.common.base.Stopwatch
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+
+import scala.util.Random
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class HDFSFileAppender(val bufferSize: Int, val timeBetweenFlushes: Long, val path: String,
+                       val total: Int) {
+  val buffer = new Array[Byte](bufferSize)
+  new Random().nextBytes(buffer)
+  val dfs = new Path(path).getFileSystem(new Configuration())
+  val outputStream = dfs.create(new Path(path))
+  val stopWatch = new Stopwatch()
+  val timer = new Timer()
+  val countInBatch = new AtomicInteger(0)
+
+  def appendEvents(): Unit = {
+    timer.scheduleAtFixedRate(new TimerTask {
+      override def run(): Unit = {
+        stopWatch.reset()
+        outputStream.synchronized {
+          stopWatch.start()
+          outputStream.hflush()
+          stopWatch.stop()
+          println("Time for batch: " + stopWatch.elapsedMillis() + " with " + countInBatch.get())
+          countInBatch.set(0)
+        }
+      }
+    }, timeBetweenFlushes, timeBetweenFlushes)
+    (1 to total).foreach(x => {
+      outputStream.synchronized {
+        println("Writing")
+        outputStream.write(buffer)
+        countInBatch.incrementAndGet()
+      }
+    })
+  }
+}
---
+++ src/test/scala/spark/TestHDFSFileAppender.scala
@@ -0,0 +1,39 @@
+package spark
+
+import benchmarktest.HDFSFileAppender
+import org.junit.Test
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class TestHDFSFileAppender {
+
+  @Test
+  def test(): Unit = {
+    val bufferSize = System.getProperty("bufferSize", "4096")
+    val timeBetweenFlushes = System.getProperty("flushInterval", "200")
+    val path = Option(System.getProperty("path"))
+      .getOrElse(
+        throw new RuntimeException("Must specify path")
+      )
+    val total = System.getProperty("total", "100000")
+    val appender = new HDFSFileAppender(bufferSize.toInt, timeBetweenFlushes.toLong, path,
+      total.toInt)
+    appender.appendEvents()
+  }
+
+}
